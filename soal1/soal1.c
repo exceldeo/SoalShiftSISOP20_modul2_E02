dbe5fb2b9761656cb086856ec73cf6fc512f9331
exceldeo <excel.deo99@gmail.com>
2020-03-12 07:49:19 +0700

    nomor 1 fix

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -11,6 +11,25 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
+  int cek = 0;
+  char cekdetik[5]="*";
+  for(int i = 60 ; i >= 0 ; --i ){
+    if(strcmp(argv[1], cekdetik) == 0){
+      cek = 1;
+      break;
+    }
+
+    if(strcmp(argv[1], cekdetik) == 0){
+      cek = 1;
+      break;
+    }
+
+    if(strcmp(argv[3], cekdetik) == 0 && i < 24){
+      cek = 1;
+      break;
+    }
+    sprintf( cekdetik, "%d", i );
+  }
   pid_t pid, sid;        // Variabel untuk menyimpan PID
 
   pid = fork();     // Menyimpan PID dari Child Process
@@ -45,24 +64,12 @@ int main(int argc, char *argv[]) {
   while (1) {
       char temp[100] = "/home/excel/Desktop/SoalShiftSISOP20_modul2_E02/soal1/",temp2[100];
       pid_t child_id;
-      // time_t t ; 
-      //   struct tm *tmp; 
-      //   char detik[2],menit[2],jam[2]; 
-      //   time( &t ); 
-        
-      //   tmp = localtime( &t );
-      //   strftime(detik, sizeof(detik), "%S", tmp);
-      //   strftime(menit, sizeof(menit), "%M", tmp);
-      //   strftime(jam, sizeof(jam), "%H", tmp);
-        // menit = localtime( &t );
-        // strftime(MY_TIME, sizeof(MY_TIME), "%M", menit);
-        // jam = localtime( &t ); 
-        // strftime(MY_TIME, sizeof(MY_TIME), "%H", jam);
         time_t rawtime;
         struct tm * timeinfo;
 
       time ( &rawtime );
       timeinfo = localtime ( &rawtime );
+
 
       // sprintf(output, "[%d %d %d %d:%d:%d]",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
       char detik[10],menit[10],jam[10];
@@ -77,63 +84,72 @@ int main(int argc, char *argv[]) {
 
       child_id = fork();
       
-      if (child_id < 0) {
+      if (child_id < 0 || cek == 0) {
+        printf("error\n");
         exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
       }
-      // if(strcmp(argv[1], detik) == 0 && strcmp(argv[2], menit) == 0 && strcmp(argv[3], jam) == 0)
+
+        // if (child_id == 0 ) {
+        //   char *argv2[] = {"touch",temp2, NULL};
+        //   execv("/usr/bin/touch", argv2);
+        // }
+
+
+      if(strcmp(argv[1], detik) == 0 && strcmp(argv[2], menit) == 0 && strcmp(argv[3], jam) == 0){
         if (child_id == 0 ) {
           char *argv2[] = {"touch",temp2, NULL};
           execv("/usr/bin/touch", argv2);
         }
-      
-      // if(argv[1] == "*" && argv[2] == "*" && argv[3] == "*"){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[1] == "*" && argv[2] == "*" )
-      // if(argv[3] == jam){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[1] == "*" && argv[3] == "*" )
-      // if(argv[2] == menit){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[3] == "*" && argv[2] == "*" )
-      // if(argv[1] == detik){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[1] == "*")
-      // if(argv[2] == menit && argv[3] == jam){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[2] == "*")
-      // if(argv[1] == detik && argv[3] == jam){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
-      // if(argv[3] == "*")
-      // if(argv[2] == menit && argv[2] == detik){
-      //   if (child_id == 0 ) {
-      //     char *argv2[] = {"touch",temp2, NULL};
-      //     execv("/usr/bin/touch", argv2);
-      //   }
-      // }
+      }
+      else if(strcmp(argv[1], "*") == 0 && strcmp(argv[2], "*") == 0 && strcmp(argv[3], "*") == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
+      else if(strcmp(argv[1], "*") == 0 && strcmp(argv[2], "*") == 0){
+        if(strcmp(argv[3], jam) == 0){
+          if (child_id == 0 ) {
+            char *argv2[] = {"touch",temp2, NULL};
+            execv("/usr/bin/touch", argv2);
+          }
+        }
+      }
+      else if(strcmp(argv[1], "*") == 0 && strcmp(argv[3], "*") == 0)
+      if(strcmp(argv[2], menit) == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
+      else if(strcmp(argv[3], "*") == 0 && strcmp(argv[2], "*") == 0)
+      if(strcmp(argv[1], detik) == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
+      else if(strcmp(argv[1], "*") == 0)
+      if(strcmp(argv[2], menit) == 0 && strcmp(argv[3], jam) == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
+      else if(strcmp(argv[2], "*") == 0)
+      if(strcmp(argv[1], detik) == 0 && strcmp(argv[3], jam) == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
+      else if(strcmp(argv[3], "*") == 0)
+      if(strcmp(argv[2], menit) == 0 && strcmp(argv[1], detik) == 0){
+        if (child_id == 0 ) {
+          char *argv2[] = {"touch",temp2, NULL};
+          execv("/usr/bin/touch", argv2);
+        }
+      }
     sleep(1);
   }
 }
